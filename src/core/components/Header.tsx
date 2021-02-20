fff460deaef2be7e2721f4ca47aad0ccec267817
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-20 15:07:50 +0800

    added header layout

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -1,5 +1,15 @@
+import dp from '../assets/dp.jpg';
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h1>An's Chatbot</h1>
+      <span>
+        <img src={dp} alt="Display Picture" />
+        <label>Jay Min</label>
+      </span>
+    </header>
+  );
 }
 
 export { Header };
