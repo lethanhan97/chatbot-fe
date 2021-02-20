b15a7416b9528b8204efe278a64c2894aecb8231
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-20 16:07:12 +0800

    finished side nav styling

--- src/core/components/SideNav.tsx
+++ src/core/components/SideNav.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
 import { getRandomColor } from '../utils';
 import './SideNav.scss';
 
+function SideNavItem({
+  item,
+  isSelected,
+  setSelectedItem,
+}: {
+  item: SideNavItem;
+  isSelected: boolean;
+  setSelectedItem: (id: number) => void;
+}) {
+  const [bgColor, setBgColor] = useState('#fff');
+  useEffect(() => {
+    setBgColor(getRandomColor());
+  }, []);
+
+  return (
+    <article
+      className={`side-nav-item-wrapper ${
+        isSelected ? 'selected-wrapper' : null
+      }`}
+      onClick={() => setSelectedItem(item.id)}
+    >
+      <div
+        style={{ backgroundColor: bgColor }}
+        className={isSelected ? 'selected-avatar' : ''}
+      >
+        {item.avatarText}
+      </div>
+      <label className={isSelected ? 'selected-text' : ''}>
+        {item.displayText}
+      </label>
+    </article>
+  );
+}
+
 function SideNav() {
+  const [selectedItem, setSelectedItem] = useState(1);
   const sideNavItems: SideNavItem[] = [
     {
       id: 1,
@@ -20,21 +56,15 @@ function SideNav() {
     },
   ];
 
-  const SideNavItem = ({ item }: { item: SideNavItem }) => {
-    const bgColor = getRandomColor();
-
-    return (
-      <article className="side-nav-item-wrapper">
-        <div style={{ backgroundColor: bgColor }}>{item.avatarText}</div>
-        <label>{item.displayText}</label>
-      </article>
-    );
-  };
-
   return (
     <section className="side-nav-wrapper">
       {sideNavItems.map((item, i) => (
-        <SideNavItem item={item} key={i} />
+        <SideNavItem
+          item={item}
+          isSelected={selectedItem === item.id}
+          setSelectedItem={setSelectedItem}
+          key={i}
+        />
       ))}
     </section>
   );
