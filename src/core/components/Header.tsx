e142f7044bde5f27e115a5cbb3bd837fdf2bcabb
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-20 16:17:58 +0800

    added hamburger icon

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -1,12 +1,17 @@
 import dp from '../assets/dp.jpg';
 import './Header.scss';
+import { IoMenuOutline as MenuIcon } from 'react-icons/io5';
 
 function Header() {
   return (
     <header className="app-header">
-      <h1>
-        <span className="app-logo">An</span>'s Chatbot
-      </h1>
+      <div className="app-title">
+        <MenuIcon size="2rem" />
+        <h1>
+          <span className="app-logo">An</span>'s Chatbot
+        </h1>
+      </div>
+
       <div className="app-avatar">
         <img src={dp} alt="Display Picture" />
         <label>Jay Min</label>
